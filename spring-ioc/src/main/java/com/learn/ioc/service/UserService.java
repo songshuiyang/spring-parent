89e71944996635776f5c0f0d84ca97b4e80e6ffe
songshuiyang <1459074711@qq.com>
2017-08-21 16:31:32 +0800

    解决冲突

---
+++ spring-ioc/src/main/java/com/learn/ioc/service/UserService.java
@@ -0,0 +1,11 @@
+package com.learn.ioc.service;
+
+/**
+ * @author songshuiyang
+ * @title:
+ * @description:
+ * @date 2017/8/21 15:24
+ */
+public interface UserService {
+    String getName();
+}
---
+++ spring-ioc/src/main/java/com/learn/ioc/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.learn.ioc.service.impl;
+
+import com.learn.ioc.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author songshuiyang
+ * @title:
+ * @description:
+ * @date 2017/8/21 15:26
+ */
+@Service
+public class UserServiceImpl implements UserService{
+
+    public String getName() {
+        return null;
+    }
+}
