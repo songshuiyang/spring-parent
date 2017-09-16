bb0b55695d7fa1a55dfb9e5dc2f0838b82de169a
songshuiyang <1459074711@qq.com>
2017-09-17 00:46:50 +0800

    Struts2 配置文件

--- spring-struts/src/main/java/com/learn/struts/action/LoginAction.java
+++ spring-struts/src/main/java/com/learn/struts/action/LoginAction.java
@@ -12,14 +12,17 @@ import org.springframework.stereotype.Component;
  * @date 2017/9/16 20:41
  */
 @Component
-public class LoginAction extends ActionSupport {
-
+public class LoginAction extends ActionSupport { // ActionSupport 默认的Action处理类
+    // 提供实例变量来封装Http请求参数
     private String userName;
 
     private String passWord;
+    // 封装服务器提示的tip实例变量（封装处理结果）
+    private String tip;
 
     public User user;
 
+    // 系统是通过对应的getter setter方法来处理请求参数的，而不是通过实例变量名来处理请求参数
     public String getUserName() {
         return userName;
     }
@@ -44,11 +47,20 @@ public class LoginAction extends ActionSupport {
         this.user = user;
     }
 
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
     // 定义处理用户请求的execute方法
     @Override
     public String execute() throws Exception {
         if (passWord.equals("123456")) {
             ActionContext.getContext().getSession().put("user", userName);
+            tip = "tip messages";
             return SUCCESS;
         } else {
             return ERROR;
--- spring-struts/src/main/webapp/welcome.jsp
+++ spring-struts/src/main/webapp/welcome.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
 <%@ page language="java" contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
 <html>
 <head>
@@ -5,6 +6,15 @@
 </head>
 <body>
 <%= session.getAttribute("user").toString() %>
+<br/>
+使用Struts2标签既可以输出Http请求参数也可以输出Action的处理结果
+<br/>
+<s:property value="tip"/>
+<br/>
+<s:property value="userName"/>
+<br/>
+<s:property value="passWord"/>
+<br/>
 登入成功
 </body>
 </html>
