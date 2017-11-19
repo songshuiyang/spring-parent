655f722bfa82893f6976d5028c5af91e93dfd6ef
songshuiyang <1459074711@qq.com>
2017-11-19 20:56:50 +0800

    初始化spring-shiro项目

---
+++ spring-shiro/src/main/java/com/shiro/learn/domain/User.java
@@ -0,0 +1,10 @@
+package com.shiro.learn.domain;
+
+/**
+ * @author songshuiyang
+ * @title:
+ * @description:
+ * @date 2017/11/18 21:22
+ */
+public class User {
+}
---
+++ spring-shiro/src/main/java/com/shiro/learn/test/LoginLogoutTest.java
@@ -0,0 +1,10 @@
+package com.shiro.learn.test;
+
+/**
+ * @author songshuiyang
+ * @title: 登录/退出
+ * @description:
+ * @date 2017/11/19 20:55
+ */
+public class LoginLogoutTest {
+}
