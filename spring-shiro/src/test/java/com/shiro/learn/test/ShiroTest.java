241996a06fad5d7f07bf99283e99049e7422193f
songshuiyang <1459074711@qq.com>
2017-11-27 21:10:50 +0800

    testBase

---
+++ spring-shiro/src/test/java/com/shiro/learn/test/ShiroTest.java
@@ -0,0 +1,46 @@
+package com.shiro.learn.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+/**
+ * @author songshuiyang
+ * @title:
+ * @description:
+ * @date 2017/11/27 20:52
+ */
+public class ShiroTest {
+
+    private Subject login(String userName, String password) {
+        SecurityManager securityManager = new IniSecurityManagerFactory("classpath:shiro/shiro1.ini").getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(userName, password);
+        try {
+            subject.login(usernamePasswordToken);
+        } catch (UnknownAccountException e) {
+            System.out.println("用户名不存在");
+        } catch (IncorrectCredentialsException e) {
+            System.out.println("用户密码不存在");
+        }
+        return subject;
+    }
+
+    @Test
+    public void testBase() {
+        Subject subject = login("song", "123");
+        PrincipalCollection collection = subject.getPrincipals();
+        System.out.println(collection.asList());
+        System.out.println(collection.getRealmNames());
+        System.out.println(subject.getPrincipal());
+    }
+
+
+}
